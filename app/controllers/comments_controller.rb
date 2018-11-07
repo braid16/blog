f7cfe7fdfecffd6f69e68880812014a5960d6725
brucevim <brucevim@protonmail.com>
2018-11-06 22:30:12 -0500

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+
+        http_basic_authenticate_with name: "ajf", password: "secr", only: [:destroy]
+
+	def create
+		@post = Post.find(params[:post_id])
+	 	@comment = @post.comments.create(params[:comment].permit(:name, :comment))
+		redirect_to post_path(@post)	
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
+	end
+end
